192823915bdf51f5013ab079476734895ec380f9
lucas22490 <battle_net90@yahoo.com>
2014-10-16 16:57:02 -0400

    Skin Changer

--- skins.lua
+++ skins.lua
@@ -3,37 +3,35 @@
 
 
 local version = 1.3
-local autoupdateenabled = true
-local UPDATE_SCRIPT_NAME = "skins"
+local AUTOUPDATE = true
+local UPDATE_NAME = "skins"
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/lucas224900/BoL/master/skins.lua"
+local UPDATE_PATH = "/lucas224900/BoL/master/skins.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-local ServerData
-if autoupdateenabled then
- GetAsyncWebResult(UPDATE_HOST, UPDATE_PATH.."?rand="..math.random(1,1000), function(d) ServerData = d end)
- function update()
-  if ServerData ~= nil then
-   local ServerVersion
-   local send, tmp, sstart = nil, string.find(ServerData, "local version = \"")
-   if sstart then
-    send, tmp = string.find(ServerData, "\"", sstart+1)
-   end
-   if send then
-    ServerVersion = tonumber(string.sub(ServerData, sstart+1, send-1))
-   end
-
-   if ServerVersion ~= nil and tonumber(ServerVersion) ~= nil and tonumber(ServerVersion) > tonumber(version) then
-    DownloadFile(UPDATE_URL.."?rand="..math.random(1,1000), UPDATE_FILE_PATH, function () print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> successfully updated. Reload (double F9) Please. ("..version.." => "..ServerVersion..")</font>") end)     
-   elseif ServerVersion then
-    print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> You have got the latest version: <u><b>"..ServerVersion.."</b></u></font>")
-   end  
-   ServerData = nil
-  end
- end
- AddTickCallback(update)
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>"..UPDATE_NAME..":</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH, "", 5)
+	if ServerData then
+		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+		if ServerVersion then
+			ServerVersion = tonumber(ServerVersion)
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available"..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	 
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
 end
+
+
 
 
 local championSkins = { ["Aatrox"] = {"Classic", "Justicar", "Mecha"}, 
